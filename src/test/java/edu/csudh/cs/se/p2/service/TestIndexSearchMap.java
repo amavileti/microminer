b7b29374e200bd7b80b2e261fd8c4099be89762f
amavileti <amavileti1@toromail.csudh.edu>
2013-04-07 13:13:02 -0700

    First Change

---
+++ src/test/java/edu/csudh/cs/se/p2/service/TestIndexSearchMap.java
@@ -0,0 +1,38 @@
+package edu.csudh.cs.se.p2.service;
+
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.when;
+
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.google.common.collect.Maps;
+
+import edu.csudh.cs.se.p2.repository.UrlRepository;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestIndexSearchMap {
+
+    private IndexSearchMapImpl impl;
+    
+    @Mock private UrlRepository urlRepository;
+    Map<String, String> sourceData;
+    
+    @Before
+    public void setUp(){        
+        sourceData = Maps.newHashMap();
+        when(urlRepository.loadUrls()).thenReturn(sourceData);
+        impl = new IndexSearchMapImpl(urlRepository);
+    }
+    
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
